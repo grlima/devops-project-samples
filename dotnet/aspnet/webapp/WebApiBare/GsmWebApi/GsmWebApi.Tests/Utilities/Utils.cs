286e89c624dfca8856b6aaa3916ddc4100f9cff2
Gustavo Lima <guslima@microsoft.com>
2018-12-29 02:25:52 -0800

    uni tests: Test Utils

---
+++ dotnet/aspnet/webapp/WebApiBare/GsmWebApi/GsmWebApi.Tests/Utilities/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace GsmWebApi.Tests.Utilities
+{
+    public static class Utils
+    {
+        public static string GetFileContents(string fileName)
+        {
+            var asm = Assembly.GetExecutingAssembly();
+            var resource = string.Format("GsmWebApi.Tests.TestData.{0}", fileName);
+            using (var stream = asm.GetManifestResourceStream(resource))
+            {
+                if (stream != null)
+                {
+                    var reader = new StreamReader(stream);
+                    return reader.ReadToEnd();
+                }
+            }
+
+            throw new Exception(string.Format("Could not retrieve resource {0}", resource));
+        }
+    }
+}
