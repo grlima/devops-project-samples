15759e907837a49f4451a5e5eea1b0cb0772dbab
Gustavo Lima <guslima@microsoft.com>
2018-12-29 03:26:48 -0800

    Unit tests: Add WebAPI nuget packages

---
+++ dotnet/aspnet/webapp/WebApiBare/GsmWebApi/GsmWebApi.Tests/Controllers/ValidationControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using GsmWebApi.Tests.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GsmWebApi.Tests.Controllers
+{
+    [TestClass]
+    public class ValidationControllerTests
+    {
+        [TestMethod]
+        public void ValidateWebTestValid()
+        {
+            // Arrange
+            ValidationController controller = new ValidationController();
+            VsWebTest vsWebTest = new VsWebTest
+            {
+                Id = "VSWebTestValid",
+                TestIntervalInSeconds = 300,
+                ApplicationId = "myapp",
+                SubscriptionId = Guid.NewGuid(),
+                InstrumentationKey = Guid.NewGuid(),
+                Name = "sometest",
+                IsRetryEnabled = true,
+                ConfigXml = Utils.GetFileContents("ValidWebTest.xml")
+            };
+
+            // Act
+            var result = controller.ValidateVsWebTest(vsWebTest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+        }
+    }
+}
