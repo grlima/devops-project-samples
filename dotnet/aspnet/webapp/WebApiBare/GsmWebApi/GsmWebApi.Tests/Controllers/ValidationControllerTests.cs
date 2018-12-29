96d3963ecfc5e2b5b8592ea38b9ba01e3209e3d9
Gustavo Lima <guslima@microsoft.com>
2018-12-29 03:47:51 -0800

    Added more unit tests

--- dotnet/aspnet/webapp/WebApiBare/GsmWebApi/GsmWebApi.Tests/Controllers/ValidationControllerTests.cs
+++ dotnet/aspnet/webapp/WebApiBare/GsmWebApi/GsmWebApi.Tests/Controllers/ValidationControllerTests.cs
@@ -33,5 +33,46 @@ namespace GsmWebApi.Tests.Controllers
             // Assert
             Assert.IsInstanceOfType(result, typeof(OkResult));
         }
+
+        [TestMethod]
+        public void ValidateWebTestWithLoop()
+        {
+            // Arrange
+            ValidationController controller = new ValidationController();
+            VsWebTest vsWebTest = new VsWebTest
+            {
+                Id = "VSWebTestValid",
+                TestIntervalInSeconds = 300,
+                ApplicationId = "myapp",
+                SubscriptionId = Guid.NewGuid(),
+                InstrumentationKey = Guid.NewGuid(),
+                Name = "sometest",
+                IsRetryEnabled = true,
+                ConfigXml = Utils.GetFileContents("InvalidWebTestWithLoop.xml")
+            };
+
+            // Act
+            var result = controller.ValidateVsWebTest(vsWebTest);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestErrorMessageResult));
+            var specificResult = result as BadRequestErrorMessageResult;
+            Assert.AreEqual("Loops are not supported.", specificResult.Message);
+        }
+
+        [TestMethod]
+        public void ValidateWebTestNullWebtest()
+        {
+            // Arrange
+            ValidationController controller = new ValidationController();
+
+            // Act
+            var result = controller.ValidateVsWebTest(null);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(BadRequestErrorMessageResult));
+            var specificResult = result as BadRequestErrorMessageResult;
+            Assert.AreEqual("No webtest found in the request body.", specificResult.Message);
+        }
     }
 }
